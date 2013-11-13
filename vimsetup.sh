8aef203653970ef96325ed6fa097fa6fc6ddc3a1
Gary L <gwleong@gmail.com>
2013-11-13 00:03:10 +0000

    added some more vim plugins and cleaned up pyflake

--- vimsetup.sh
+++ vimsetup.sh
@@ -1,4 +1,4 @@
-#tmkdir dotfiles
+##importing exampltmkdir dotfiles
 #cd dotfiles
 #mkdir bash bin desktop utils vim
 #cd vim/
@@ -20,8 +20,9 @@ cd ~/dotfiles
 git submodule add git://github.com/tpope/vim-fugitive.git vim/bundle/fugitive
 git submodule init && git submodule update
 
+#git submodule add git://github.com/georgexsh/pyflakes-vim.git vim/bundle/pyflakes-vim
 cd ~/dotfiles
-git submodule add git://github.com/georgexsh/pyflakes-vim.git vim/bundle/pyflakes-vim
+git submodule add git://github.com/kevinw/pyflakes-vim.git vim/bundle/pyflakes-vim
 cd vim/bundle/pyflakes-vim/
 git submodule init && git submodule update
 
@@ -70,6 +71,33 @@ git submodule add git://github.com/vim-scripts/ctags.vim--Johnson.git vim/bundle
 git submodule init && git submodule update
 
 
+cd ~/dotfiles
+git submodule add https://github.com/wincent/Command-T.git vim/bundle/command-t
+git submodule init && git submodule update
+
+
+
+#git submodule add http://github.com/tpope/vim-fugitive.git vim/bundle/fugitive
+#git submodule add https://github.com/msanders/snipmate.vim.git vim/bundle/snipmate
+#git submodule add https://github.com/tpope/vim-surround.git vim/bundle/surround
+#git submodule add https://github.com/tpope/vim-git.git vim/bundle/git
+#git submodule add https://github.com/ervandew/supertab.git vim/bundle/supertab
+#git submodule add https://github.com/sontek/minibufexpl.vim.git vim/bundle/minibufexpl
+#git submodule add https://github.com/wincent/Command-T.git vim/bundle/command-t
+#git submodule add https://github.com/mitechie/pyflakes-pathogen.git
+#git submodule add https://github.com/mileszs/ack.vim.git vim/bundle/ack
+#git submodule add https://github.com/sjl/gundo.vim.git vim/bundle/gundo
+#git submodule add https://github.com/fs111/pydoc.vim.git vim/bundle/pydoc
+#git submodule add https://github.com/vim-scripts/pep8.git vim/bundle/pep8
+#git submodule add https://github.com/alfredodeza/pytest.vim.git vim/bundle/py.test
+#git submodule add https://github.com/reinh/vim-makegreen vim/bundle/makegreen
+#git submodule add https://github.com/vim-scripts/TaskList.vim.git vim/bundle/tasklist
+#git submodule add https://github.com/vim-scripts/The-NERD-tree.git vim/bundle/nerdtree
+#git submodule add https://github.com/sontek/rope-vim.git vim/bundle/ropevim
+#git submodule init
+#git submodule update
+#git submodule foreach git submodule init
+#git submodule foreach git submodule update
 
 
 
